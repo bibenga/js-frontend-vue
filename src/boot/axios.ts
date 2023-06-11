ae110ef7905808467c2b562be4f5cf06ff7586ad
bibenga <{ID}+{username}@users.noreply.github.com>
2023-06-11 14:39:58 +0000

    updates formating

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -4,49 +4,51 @@ import axios, { AxiosInstance } from 'axios'
 import { inject } from 'vue'
 
 export default boot(({ app, ssrContext }) => {
-    console.log(`[axios] init: SERVER=${process.env.SERVER}, ssrContext is null=${ssrContext == null}`)
+  console.log(`[axios] init: SERVER=${process.env.SERVER}, ssrContext is null=${ssrContext == null}`)
 
-    let baseURL = ''
-    if (ssrContext == null) {
-        baseURL = '/data'
-    } else {
-        baseURL = 'http://127.0.0.1:9100/data'
+  let baseURL = ''
+  if (ssrContext == null) {
+    baseURL = '/data'
+  } else {
+    baseURL = 'http://127.0.0.1:9100/data'
+  }
+  const $api: AxiosInstance = axios.create({
+    baseURL: baseURL,
+    xsrfCookieName: 'csrftoken',
+    xsrfHeaderName: 'X-XSRF-TOKEN'
+  })
+
+  const cookies = ssrContext != null ? Cookies.parseSSR(ssrContext) : Cookies
+
+  $api.defaults.headers.common['X-Api-Key-Api'] = 'Api';
+  $api.interceptors.request.use(
+    config => {
+      config.headers['X-Api-Key-Api2'] = 'Api2';
+      const session = cookies.get('session')
+      if (session) {
+        config.headers.XSessionId = `${session}`
+      }
+      return config;
+    },
+    error => {
+      return Promise.reject(error);
     }
-    const $api: AxiosInstance = axios.create({
-        baseURL: baseURL,
-    })
+  );
 
-    const cookies = ssrContext != null ? Cookies.parseSSR(ssrContext) : Cookies
-
-    $api.defaults.headers.common['X-Api-Key-Api'] = 'Api';
-    $api.interceptors.request.use(
-        config => {
-            config.headers['X-Api-Key-Api2'] = 'Api2';
-            const session = cookies.get('session')
-            if (session) {
-                config.headers.XSessionId = `${session}`
-            }
-            return config;
-        },
-        error => {
-            return Promise.reject(error);
-        }
-    );
-
-    // for use inside Vue files (Options API) through inject<>(), this.$axios and this.$api
-    app.config.globalProperties.$api = $api // this.$api
-    app.provide('$api', $api) // const $api = inject<AxiosInstance>('$api')
-    // app.config.globalProperties.$axios = axios // this.$axios
-    // app.provide('$axios', axios) // const $axios = inject<Axios>('$axios')
+  // for use inside Vue files (Options API) through inject<>(), this.$axios and this.$api
+  app.config.globalProperties.$api = $api // this.$api
+  app.provide('$api', $api) // const $api = inject<AxiosInstance>('$api')
+  // app.config.globalProperties.$axios = axios // this.$axios
+  // app.provide('$axios', axios) // const $axios = inject<Axios>('$axios')
 })
 
 
 function useApi(): AxiosInstance {
-    const $api = inject<AxiosInstance>('$api')
-    if ($api === undefined) {
-        throw Error('$api is not defined')
-    }
-    return $api
+  const $api = inject<AxiosInstance>('$api')
+  if ($api === undefined) {
+    throw Error('$api is not defined')
+  }
+  return $api
 }
 
 export { useApi }
--- src/boot/simpleauth.ts
+++ src/boot/simpleauth.ts
@@ -3,28 +3,28 @@ import { useAuthStore } from 'stores/auth'
 import { useProfileStore } from 'stores/profiles'
 
 export default boot(async ({ router }) => {
-    console.log('[simpleauth.init] >')
+  console.log('[simpleauth.init] >')
 
-    const authStore = useAuthStore()
-    const profileStore = useProfileStore()
+  const authStore = useAuthStore()
+  const profileStore = useProfileStore()
 
-    router.beforeEach(async (to, from, next) => {
-        console.log(`[simpleauth.beforeEach] > ${from.fullPath} -> ${to.fullPath}`)
-        if (to.path == '/login') {
-            console.log('[simpleauth.beforeEach] always pass to login')
-            next()
-            return
-        }
-        await authStore.init()
-        await profileStore.load()
-        if (authStore.user?.authenticated) {
-            console.log('[simpleauth.beforeEach] < pass')
-            next()
-            return
-        }
-        console.log('[simpleauth.beforeEach] < reject')
-        next({ path: '/login', query: { next: to.fullPath } })
-    })
+  router.beforeEach(async (to, from, next) => {
+    console.log(`[simpleauth.beforeEach] > ${from.fullPath} -> ${to.fullPath}`)
+    if (to.path == '/login') {
+      console.log('[simpleauth.beforeEach] always pass to login')
+      next()
+      return
+    }
+    await authStore.init()
+    await profileStore.load()
+    if (authStore.user?.authenticated) {
+      console.log('[simpleauth.beforeEach] < pass')
+      next()
+      return
+    }
+    console.log('[simpleauth.beforeEach] < reject')
+    next({ path: '/login', query: { next: to.fullPath } })
+  })
 
-    console.log('[simpleauth.init] <')
+  console.log('[simpleauth.init] <')
 })
