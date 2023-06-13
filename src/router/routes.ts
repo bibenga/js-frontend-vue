70ba8e16035c172f057dbf6d0c59bb0dff10d1f7
bibenga <{ID}+{username}@users.noreply.github.com>
2023-06-13 10:11:54 +0000

    update router

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -2,37 +2,20 @@ import { RouteRecordRaw } from 'vue-router';
 
 const routes: RouteRecordRaw[] = [
     {
-        path: '/demo',
-        component: () => import('layouts/MainLayout.vue'),
-        children: [{ path: '', component: () => import('pages/DemoPage.vue') }],
-    },
-
-    {
         path: '/',
-        redirect: '/dashboard'
+        component: () => import('layouts/MainLayout.vue'),
+        children: [
+            { path: '', redirect: '/dashboard' },
+            { path: 'demo', component: () => import('pages/DemoPage.vue') },
+            { path: 'dashboard', component: () => import('pages/DashboardPage.vue') },
+            { path: 'settings', component: () => import('pages/SettingsPage.vue') },
+            { path: 'profile/:profileId(\\d+)', props: true, component: () => import('pages/ProfilePage.vue') }
+        ],
     },
 
     {
         path: '/login',
         component: () => import('pages/LoginPage.vue'),
-    },
-
-    {
-        path: '/settings',
-        component: () => import('layouts/MainLayout.vue'),
-        children: [{ path: '', component: () => import('pages/SettingsPage.vue') }],
-    },
-
-    {
-        path: '/dashboard',
-        component: () => import('layouts/MainLayout.vue'),
-        children: [{ path: '', component: () => import('pages/DashboardPage.vue') }],
-    },
-
-    {
-        path: '/profile/:profileId(\\d+)',
-        component: () => import('layouts/MainLayout.vue'),
-        children: [{ path: '', props: true, component: () => import('pages/ProfilePage.vue') }],
     },
 
     // Always leave this as last one,
